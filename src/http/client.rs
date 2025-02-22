21d4f14bdd074d607800739dd1e235a3f25688d3
esther-ff <creepermaster000@gmail.com>
2025-02-22 19:59:38 +0100

    requests, pain, headers

--- src/http/client.rs
+++ src/http/client.rs
@@ -1,16 +1,102 @@
-use super::request::RequestFuture;
-use crate::tls_client::TlsClient;
+use super::request::{HeaderList, ReqBuilder, RequestFuture};
+use crate::tls_client::{Resolving, TlsClient};
 use std::collections::HashMap;
+use std::io;
+use std::pin::Pin;
+use std::task::{Context, Poll, ready};
+
+pub(crate) struct Connecting<'c> {
+    tls: Resolving<'c>,
+    user_agent: Option<&'static str>,
+    headers: Option<HeaderList<'c>>,
+}
+
+impl<'c> Future for Connecting<'c> {
+    type Output = io::Result<Client<'c>>;
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let res = ready!(Pin::new(&mut self.tls).poll(cx));
+
+        match res {
+            Ok(tls) => {
+                // Under normal circumstances this is 100% safe.
+                let user_agent = self.user_agent.take().expect("no user_agent present");
+
+                let headers = self.headers.take();
+                let client = Client {
+                    tls,
+                    user_agent,
+                    headers,
+                };
+
+                Poll::Ready(Ok(client))
+            }
+            Err(e) => Poll::Ready(Err(e)),
+        }
+    }
+}
 
 pub struct Client<'c> {
     tls: TlsClient<'c>,
     user_agent: &'static str,
-    headers: HashMap<&'c str, String>,
+    headers: Option<HeaderList<'c>>,
 }
 
-impl Client<'_> {
-    pub fn new() -> Self {
-        // create
-        Self {}
+pub enum Method {
+    GET,
+    PUT,
+    POST,
+    HEAD,
+    PATCH,
+    OPTIONS,
+    CONNECT,
+}
+
+impl Method {
+    pub(crate) const fn bytes(&self) -> &'static [u8] {
+        match *self {
+            Method::GET => "GET ".as_bytes(),
+            Method::PUT => "PUT ".as_bytes(),
+            Method::POST => "POST ".as_bytes(),
+            Method::HEAD => "HEAD ".as_bytes(),
+            Method::PATCH => "PATCH ".as_bytes(),
+            Method::OPTIONS => "OPTIONS ".as_bytes(),
+            Method::CONNECT => "CONNECT ".as_bytes(),
+        }
     }
+}
+
+impl<'c> Client<'c> {
+    pub fn connect(
+        url: &'static str,
+        user_agent: &'static str,
+        headers: Option<&'c HashMap<&'c str, String>>,
+    ) -> io::Result<Connecting<'c>> {
+        let tls = TlsClient::create(None, url)?;
+
+        let hdr = match headers {
+            None => None,
+            Some(map) => {
+                let mut hdrlist = HeaderList::new();
+                map.into_iter()
+                    .for_each(|(key, val)| hdrlist.put((key, &*val)));
+
+                Some(hdrlist)
+            }
+        };
+
+        Ok(Connecting {
+            tls,
+            user_agent: Some(user_agent),
+            headers: hdr,
+        })
+    }
+
+    pub(crate) fn get_header_slice(&self) -> Option<&[(&'c str, &'c str)]> {
+        match self.headers.as_ref() {
+            Some(h) => Some(h.header_slice()),
+            None => None,
+        }
+    }
+
+    fn execute(&self, req: ReqBuilder) {}
 }
--- src/http/request.rs
+++ src/http/request.rs
@@ -1,11 +1,14 @@
 use std::future::Future;
 use std::io;
 use std::pin::Pin;
-use std::sync::Arc;
 use std::task::{Context, Poll, ready};
 
+use super::client::{Client, Method};
 use crate::tls_client::TlsClient;
 use lamp::io::{AsyncRead, AsyncWrite};
+use std::mem::MaybeUninit;
+
+const HEADER_MAX: usize = 64;
 
 pub(crate) struct RequestFuture<'a> {
     data: &'a [u8],
@@ -49,5 +52,256 @@ impl Future for RequestFuture<'_> {
             Poll::Ready(Ok(_)) => Poll::Ready(Ok(())),
             Poll::Ready(Err(e)) => Poll::Ready(Err(e)),
         }
+    }
+}
+
+pub struct HeaderList<'h> {
+    hdr: [MaybeUninit<(&'h str, &'h str)>; HEADER_MAX],
+    cursor: usize,
+}
+
+impl<'h> HeaderList<'h> {
+    pub(crate) fn new() -> Self {
+        Self {
+            hdr: [const { MaybeUninit::uninit() }; HEADER_MAX],
+            cursor: 0,
+        }
+    }
+
+    fn can_push(&self) -> bool {
+        self.cursor - 1 != HEADER_MAX
+    }
+
+    pub(crate) fn put(&mut self, hdr: (&'h str, &'h str)) {
+        // Ensures we don't overflow.
+        // This function should be coupled with `can_push`.
+        if self.cursor - 1 == HEADER_MAX {
+            return;
+        }
+
+        let slot = unsafe { self.hdr.get_unchecked_mut(self.cursor) };
+        slot.write(hdr);
+
+        self.cursor += 1;
+    }
+
+    pub(crate) fn header_slice(&self) -> &[(&'h str, &'h str)] {
+        if self.cursor == 0 {
+            return &[];
+        }
+
+        let slice = &self.hdr[0..self.cursor - 1];
+        unsafe {
+            &*(slice as *const [MaybeUninit<(&'h str, &'h str)>] as *const [(&'h str, &'h str)])
+        }
+    }
+
+    fn dealloc(&mut self, index: usize) {
+        unsafe {
+            let slot = self.hdr.get_unchecked_mut(index);
+            slot.assume_init_drop();
+        }
+    }
+
+    fn get(&self, index: usize) -> Option<(&'h str, &'h str)> {
+        if index >= self.cursor {
+            return None;
+        }
+
+        unsafe {
+            let item = self.hdr.get_unchecked(index);
+
+            Some(item.assume_init())
+        }
+    }
+
+    fn iter(&'h self) -> impl Iterator<Item = (&'h str, &'h str)> {
+        HdrListIter { pos: 0, hdrs: self }
+    }
+}
+
+pub struct HdrListIter<'i> {
+    pos: usize,
+    hdrs: &'i HeaderList<'i>,
+}
+
+impl<'i> Iterator for HdrListIter<'i> {
+    type Item = (&'i str, &'i str);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos == self.hdrs.cursor - 1 {
+            return None;
+        }
+
+        let item = self.hdrs.get(self.pos);
+        self.pos += 1;
+        item
+    }
+}
+
+impl std::ops::Drop for HeaderList<'_> {
+    fn drop(&mut self) {
+        while self.cursor != 0 {
+            self.dealloc(self.cursor);
+            self.cursor += 1;
+        }
+
+        self.dealloc(0);
+    }
+}
+
+pub struct ReqBuilder<'b> {
+    method: Method,
+    route: Option<&'b str>,
+    headers: Option<&'b [(&'b str, &'b str)]>,
+    extra_headers: Option<HeaderList<'b>>,
+    content: Option<&'b [u8]>,
+}
+
+impl<'b> ReqBuilder<'b> {
+    const CRLF: &'static [u8] = &[b'\r', b'\n'];
+
+    pub fn new(method: Method) -> Self {
+        Self {
+            method,
+            route: None,
+            headers: None,
+            extra_headers: None,
+            content: None,
+        }
+    }
+
+    pub fn default_headers(&mut self, client: &'b Client) -> &mut Self {
+        self.headers = client.get_header_slice();
+        self
+    }
+
+    pub fn set_route(&mut self, route: &'b str) -> &mut Self {
+        self.route.replace(route);
+        self
+    }
+
+    pub fn set_content(&mut self, content: &'b [u8]) -> &mut Self {
+        self.content.replace(content);
+        self
+    }
+
+    pub fn add_headers(&mut self, iter: impl IntoIterator<Item = (&'b str, &'b str)>) -> &mut Self {
+        if self.extra_headers.is_none() {
+            self.extra_headers = Some(HeaderList::new())
+        };
+
+        let mut hdrlist = self.extra_headers.take().unwrap();
+
+        iter.into_iter().for_each(|tuple| {
+            if !hdrlist.can_push() {
+                return;
+            }
+
+            hdrlist.put(tuple);
+        });
+
+        self
+    }
+
+    pub(crate) fn construct(self) -> Vec<u8> {
+        let mut req = Vec::with_capacity(8192);
+
+        // First HTTP line.
+        req.extend_from_slice(self.method.bytes());
+
+        match self.route {
+            None => {
+                req.push(b'/');
+            }
+
+            Some(route) => {
+                req.extend_from_slice(route.as_bytes());
+            }
+        };
+
+        req.extend_from_slice(" HTTP/1.1\r\n".as_bytes());
+
+        // Headers
+        match self.headers {
+            None => {}
+            Some(slice) => {
+                slice.iter().for_each(|(k, v)| {
+                    req.extend_from_slice(k.as_bytes());
+                    req.extend_from_slice(": ".as_bytes());
+                    req.extend_from_slice(v.as_bytes());
+                    req.extend_from_slice(Self::CRLF);
+                });
+            }
+        }
+
+        // Extra headers
+        match self.extra_headers {
+            None => {}
+            Some(hdrs) => {
+                hdrs.iter().for_each(|(k, v)| {
+                    req.extend_from_slice(k.as_bytes());
+                    req.extend_from_slice(": ".as_bytes());
+                    req.extend_from_slice(v.as_bytes());
+                    req.extend_from_slice(Self::CRLF);
+                });
+            }
+        }
+
+        // Content if any
+        req.extend_from_slice(Self::CRLF);
+
+        match self.content {
+            Some(content) => {
+                req.extend(content.iter());
+            }
+
+            None => {}
+        };
+
+        req
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{HeaderList, Method, ReqBuilder};
+
+    #[test]
+    fn create_header_list() {
+        HeaderList::new();
+    }
+
+    #[test]
+    fn use_header_list() {
+        let mut hdrs = HeaderList::new();
+
+        hdrs.put(("User-Agent", "Versailles"));
+
+        let item = hdrs.get(0).expect("empty header list");
+
+        assert!("User-Agent" == item.0);
+        assert!("Versailles" == item.1);
+
+        drop(hdrs);
+    }
+
+    #[test]
+    fn req_builder() {
+        let mut hdrs = HeaderList::new();
+        hdrs.put(("User-Agent", "Versailles"));
+
+        let mut req = ReqBuilder::new(Method::GET);
+
+        req.set_route("/droit/humain")
+            .set_content("pas de dieu, pas de maitre".as_bytes())
+            .add_headers(hdrs.iter());
+
+        let bytes = req.construct();
+
+        let str_req = std::str::from_utf8(&bytes);
+        assert!(str_req.is_ok());
+
+        println!("{}", str_req.unwrap());
     }
 }
